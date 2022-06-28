ca8cf4e37e7e873e0d5d5de96e75cd3067a52e8e
milesba4 <milesbaffour@gmail.com>
2022-06-28 19:10:31 -0400

    authenticating email and password in auth.js

--- vaccine-hub-api/routes/auth.js
+++ vaccine-hub-api/routes/auth.js
@@ -1,11 +1,12 @@
 const express = require("express")
 const User = require("../models/user")
 const router = express.Router()
-const user = require("../models/user")
+// const user = require("../models/user")
 
 router.post("/login",async(req,res,next)=>{
 try{
-// take the users email and password and attempting to authenticate them
+    const user = await User.login(req.body) // take the users email and password and attempting to authenticate them
+    return res.status(200).json({user})
 
 }catch(err){
 next(err)
@@ -14,10 +15,11 @@ next(err)
 })
 router.post("/register", async(req,res,next)=>{
     try{
-        
-        //Take users email,password, rsvp status 
-        // create new user in db
+        console.log("req body=",req.body)
+        const user = await User.register(req.body)
+        return res.status(200).json({user})
         }catch(err){
+        console.log("hello")
         next(err)
         }
 })
