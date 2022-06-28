24549926f15fabae70eeb353a3b89a8108f3e6ac
milesba4 <milesbaffour@gmail.com>
2022-06-28 18:54:05 -0400

    creating bcrypt factor

--- vaccine-hub-api/config.js
+++ vaccine-hub-api/config.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT ? Number(process.env.PORT): 3001
 
 function getDatabaseUri(){
 const dbUser = process.env.DATABASE_USER || "postgres"
-const dbPass = process.env.DATABASE_PASS ? encodeURI(process.env.DATABASE_PASS): "postgres"
+const dbPass = process.env.DATABASE_PASS ? encodeURI(process.env.DATABASE_PASS): "postgres" // ask about password
 const dbHost = process.env.DATABASE_HOST || "localhost"
 const dbPort = process.env.DATABASE_PORT || 5432
 const dbName = process.env.DATABASE_NAME || "vaccine_hub"
@@ -14,6 +14,8 @@ const dbName = process.env.DATABASE_NAME || "vaccine_hub"
 //else create the dbconnection string ourselves
 return process.env.DATABASE_URL || `postgresql://${dbUser}:${dbPass}@${dbHost}:${dbPort}/${dbName}`
 }
+
+const BCRYPT_WORK_FACTOR = 13 
 console.log("Vaccine Hub Config:".red)
 console.log("PORT:".blue,PORT)
 console.log("Database URI:".blue, getDatabaseUri())
@@ -22,5 +24,6 @@ console.log("------".yellow)
 module.exports = {
 PORT,
 getDatabaseUri,
+BCRYPT_WORK_FACTOR
 
 }
