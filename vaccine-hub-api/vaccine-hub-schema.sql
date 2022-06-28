bf61fbbbd1f5dd15c63d8e5ad0d7f2480bd273a1
milesba4 <milesbaffour@gmail.com>
2022-06-28 14:27:24 -0400

    Fixing schema

--- vaccine-hub-api/vaccine-hub-schema.sql
+++ vaccine-hub-api/vaccine-hub-schema.sql
@@ -3,10 +3,9 @@ CREATE TABLE users(
     password    TEXT NOT NULL,
     first_name TEXT NOT NULL,
     last_name TEXT NOT NULL,
-    email        TEXT NOT NULL UNIQUE CHECK(POSITION('@') IN email) > 1,
+    email    TEXT NOT NULL UNIQUE CHECK (POSITION('@' IN email) > 1),
     location   TEXT NOT NULL,
     date TIMESTAMP NOT NULL DEFAULT NOW(),
     rsvp_status BOOLEAN NOT NULL,
-    num_guests INTEGER NOT NULL,
-    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
-);
+    num_guests INTEGER NOT NULL
+)
