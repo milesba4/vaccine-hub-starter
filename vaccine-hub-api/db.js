953ee206b74dfabde3a52e605484a0bf50be20cc
milesba4 <milesbaffour@gmail.com>
2022-06-28 16:15:15 -0400

    connecting to postgres and logging message to terminal

--- vaccine-hub-api/db.js
+++ vaccine-hub-api/db.js
@@ -1,3 +1,16 @@
 const{Client} = require("pg")
 const {getDatabaseUri}= require("./config")
 require("colors")
+
+const db = new Client ({connectionString: getDatabaseUri()})
+
+db.connect((err)=>{
+if(err){
+    console.error("connection error".red,err.stack)
+}else{
+    console.log("successfully connected db".blue)
+}
+
+})
+
+module.exports = db
