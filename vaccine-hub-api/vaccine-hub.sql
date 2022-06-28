31b0de782296d5f0e969ff0402eeae25e65ce2db
milesba4 <milesbaffour@gmail.com>
2022-06-28 13:45:17 -0400

    Creating users table

---
+++ vaccine-hub-api/vaccine-hub.sql
@@ -0,0 +1,12 @@
+CREATE TABLE users(
+    id      SERIAL PRIMARY KEY, 
+    password    TEXT NOT NULL,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    email        TEXT NOT NULL UNIQUE CHECK(POSITION('@') IN email) > 1,
+    location   TEXT NOT NULL,
+    date TIMESTAMP NOT NULL DEFAULT NOW(),
+    rsvp_status BOOLEAN NOT NULL,
+    num_guests INTEGER NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+);
